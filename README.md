c708a32dc3ddc5b32f9ffebe0d8628c7e7cecc6d
kalpshah485 <kalpshah485@gmail.com>
2022-03-11 11:47:25 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-g8qud4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-g8qud4)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import createSelector from 'reselect';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
